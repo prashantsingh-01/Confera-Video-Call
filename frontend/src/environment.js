7e3dd78eab5d4bc8c425ea96c524ea43aedbbe5e
Prashant Singh <prashant.singh.d3v@gmail.com>
2025-08-01 07:07:19 +0530

    Update environment.js

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD
-  ? "https://confera-video-call-backend.vercel.app/"
+  ? "https://confera-video-call-backend.vercel.app"
   : "http://localhost:8000";
 
 export default server;
